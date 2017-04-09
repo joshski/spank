91f0bf536e8b73faa3a80c2332ffa50baafdb275
Josh Chisholm <joshuachisholm@gmail.com>
2017-04-09 10:21:45 +0200

    Move URL handing to app

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const routism = require('routism')
+const parseURL = require('url').parse
+const querystring = require('querystring')
 
 function SpankApp(routes) {
   this.router = routism.compile(routes)
 }
 
 SpankApp.prototype.respond = function (request) {
-  const recognition = this.router.recognise(request.path)
+  const url = parseURL(request.url)
+  const requestParams = this.paramsFromUrl(url)
+  const recognition = this.router.recognise(url.pathname)
   if (recognition) {
     const method = recognition.route[request.method.toLowerCase()]
     if (method) {
       const params = recognition.params.reduce((params, pair) => {
         params[pair[0]] = pair[1]; return params
-      }, request.params)
+      }, requestParams)
       const result = method({ params })
       if (typeof result === 'string') {
         return {
@@ -23,6 +27,10 @@ SpankApp.prototype.respond = function (request) {
     }
   }
   return { status: 404 }
+}
+
+SpankApp.prototype.paramsFromUrl = function (url) {
+  return url.query ? querystring.parse(url.query) : {}
 }
 
 module.exports = SpankApp
--- connectAdapter.js
+++ connectAdapter.js
@@ -1,23 +1,14 @@
-const { parse } = require('url')
-const querystring = require('querystring')
-
 function ConnectAdapter() {}
 
 ConnectAdapter.prototype.connectify = function(spankApp) {
   return (request, response) => {
-    const url = parse(request.url)
-    const params = this.paramsFromUrl(url)
-    const spankRequest = { method: request.method, path: url.pathname, params }
+    const spankRequest = { method: request.method, url: request.url }
     const spankResponse = spankApp.respond(spankRequest)
     if (spankResponse) {
       response.writeHead(spankResponse.status, spankResponse.headers)
       response.end(spankResponse.body)
     }
   }
-}
-
-ConnectAdapter.prototype.paramsFromUrl = function (url) {
-  return url.query ? querystring.parse(url.query) : {}
 }
 
 module.exports = ConnectAdapter
